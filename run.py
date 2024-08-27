8bc6cced806113ec68e1d1dd066b9491fc44a682
neo050 <neoray.asking666@gmail.com>
2024-08-27 07:12:05 +0300

    Modify run.py to listen on all interfaces:

--- run.py
+++ run.py
@@ -2,7 +2,7 @@
 
 from app_api import create_app
 
-app = create_app()
+if __name__ == "__main__":
+    app = create_app()
+    app.run(host='0.0.0.0', port=5000)  # Bind to all interfaces
 
-if __name__ == '__main__':
-    app.run(debug=True)
